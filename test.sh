0c7077c264989a2955cf960009ee2a9f238838c9
sravanthi <sravanthi.annam25@gmail.com>
2023-07-09 18:40:02 +0530

    sravanthi conflict added

--- test.sh
+++ test.sh
@@ -1,5 +1,12 @@
  added one line for moving
- adding another line
+ This is the official Uptodown application specifically designed for the Android operating system. With it, users will download apps in APK format quickly and safely. Plus, they will get automatic updates as well as the option to roll back to any previous version.
+
+Uptodown is a totally open app store without any regional or country-specific restrictions. Plus, you don't need to go through any sign-up processes or subscriptions here. In fact, you don't even need a Google Play account. And on top of allowing app downloads and APK installs from our own servers, the Uptodown application also detects XAPK files with additional OBB data. You can make backups for any apps you download, and pick and choose which ones you want to be updated —all from the comfort of a simple and trustworthy interface.
+
+Along with its huge catalog of thousands of Android apps, Uptodown App Store provide enriched editorial content on apps, including in-house screenshots and videos we make ourselves. Our content is written by our team of writers, with neutrality at the cornerstone of our services.
+
+Uptodown for Android is a much-improved version of our web-browser edition, with all the unique features any Android smartphone and tablet user require to install, manage and enjoy the most extensive alternative catalog of appeiser:adding another line
  move one line for testing
  this is new branch
-new repositary
+new repositar
+added  lines
